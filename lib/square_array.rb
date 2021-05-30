1cd1ec12d89ebc0883ad46be2aa27320662cb232
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-30 17:05:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,27 @@
-def square_array(array)
-  # your code here
+#def square_array
+#  numbers = [1, 2, 3]
+#  count = 0
+#  while count < numbers.length do
+#    numbers[count] = numbers[count] ** 2
+#    count += 1
+#    p numbers[count]
+#  end
+#end
+
+
+def square_array(numbers)
+  new_numbers = []
+  count = 0 
+  while count < array.length do
+    
+  end 
+  new_numbers << (num ** 2)
+  return new_numbers
 end
+def square_array(numbers)
+    new_numbers = []
+    numbers.map do |num|
+      new_numbers << (num ** 2)
+    end
+    return new_numbers
+  end
